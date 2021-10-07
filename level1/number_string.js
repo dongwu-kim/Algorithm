c72a136e958857dedc5f9feb75233322b2f4f81b
dongwu-kim <alduk7797@naver.com>
2021-10-07 12:57:46 +0900

    [ADD] : level1 문제풀이 - 숫자 문자열과 영단어

---
+++ level1/number_string.js
@@ -0,0 +1,33 @@
+function solution(s) {
+  var answer = 0;
+
+  s = s
+    .replace(/one/g, "1")
+    .replace(/two/g, "2")
+    .replace(/three/g, "3")
+    .replace(/four/g, "4")
+    .replace(/five/g, "5")
+    .replace(/six/g, "6")
+    .replace(/seven/g, "7")
+    .replace(/eight/g, "8")
+    .replace(/nine/g, "9")
+    .replace(/zero/g, "0");
+
+  answer = parseInt(s, 10);
+
+  return answer;
+}
+
+// 인상깊던 풀이법
+
+function solution(s) {
+  let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+  var answer = s;
+
+  for (let i = 0; i < numbers.length; i++) {
+    let arr = answer.split(numbers[i]);
+    answer = arr.join(i);
+  }
+
+  return Number(answer);
+}
